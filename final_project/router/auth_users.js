a13749c2f2aa36913cf540d1802623d10fe661a6
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-01-13 16:58:09 -0600

    Add POST method handling logic to /customer/login segment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,13 +17,58 @@ const isValid = (username) => { //returns boolean
 
 const authenticatedUser = (username, password) => { //returns boolean
   //write code to check if username and password match the one we have in records.
+  const found_user = users.find(
+    user => (
+      user["username"] === username &&
+      user["password"] === password
+    )
+  );
+
+  if (found_user) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+regd_users.post(
+  "/login",
+  (req, res) => {
+    const { username, password } = req.body;
+
+    if (
+      username &&
+      password
+    ) {
+      if (authenticatedUser(username, password)) {
+
+        const accessToken = jwt.sign(
+          {
+            data: password
+          },
+          "access",
+          {
+            expiresIn: 60 * 60
+          }
+        );
+
+        req.session.authorization = {
+          accessToken
+        }
+
+        res.send("User successfully logged in");
+      }
+      else {
+        res.send("Either the username or password is incorrect!");
+      }
+    }
+    else {
+      res.send("Both username and password are required!");
+    }
+  }
+);
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
