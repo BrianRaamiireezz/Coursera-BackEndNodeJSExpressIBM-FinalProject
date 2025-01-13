ac5068505994122c5ad6beef71a7d9343af760a7
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-01-13 15:26:15 -0600

    Add GET method handling logic to /title/:title segment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -79,10 +79,23 @@ public_users.get(
 );
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get(
+  '/title/:title',
+  function (req, res) {
+    const title = req.params.title;
+    let found_book = {};
+
+    if (
+      title &&
+      _get_book_by_attribute("title", title, found_book)
+    ) {
+      res.json({ book: found_book });
+    }
+    else {
+      res.send("Unable to find the book!");
+    }
+  }
+);
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
