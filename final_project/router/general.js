3f47b33cb7cff6c1f9a91c28f0a17297ad61b41e
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-01-13 15:17:31 -0600

    Add GET method handling logic to /author/:author segment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,11 +36,47 @@ public_users.get(
   }
 );
 
+// Util function
+function _get_book_by_attribute(attribute, value, result) {
+  const found_book =
+    Object.values(books).find(
+      book => (
+        book[attribute] === value
+      )
+    );
+
+  if (result) {
+    Object.keys(found_book).forEach(
+      key => {
+        result[key] = found_book[key]
+      }
+    );
+
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get(
+  '/author/:author',
+  function (req, res) {
+    const author = req.params.author;
+    let found_book = {};
+
+    if (
+      author &&
+      _get_book_by_attribute("author", author, found_book)
+    ) {
+      res.json({ book: found_book });
+    }
+    else {
+      res.send("Unable to find the book!");
+    }
+  }
+);
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
