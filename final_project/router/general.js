877599b501de96bae587134bcda5d7fc1133ab1d
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-01-13 14:38:04 -0600

    Add GET method handling logic to /isbn/:isbn segment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,10 +19,22 @@ public_users.get(
 );
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get(
+  '/isbn/:isbn',
+  function (req, res) {
+    const isbn = req.params.isbn;
+
+    if (
+      isbn &&
+      Object.keys(books).includes(isbn)
+    ) {
+      res.json(books[isbn]);
+    }
+    else {
+      res.send("Unable to find the book!");
+    }
+  }
+);
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
